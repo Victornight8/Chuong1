120cdf4af06bf68f42bb170fe7fb851bc9766498
Victornight8 <154253786+Victornight8@users.noreply.github.com>
2025-02-21 23:33:06 +0700

    Update bai45.cs

--- bai45.cs
+++ bai45.cs
@@ -1,32 +1,26 @@
-﻿using System;
+using System;
 
-class bai37
+class Bai45
 {
     static void Main()
     {
-        int i_136, n_136;
-        double S_136;
+        int themang_136, n_136, P_136;
 
         do
         {
-            Console.Write("\nNhap n (n >= 2): ");
+            Console.Write("\nNhap n: ");
             n_136 = int.Parse(Console.ReadLine());
+        } while (n_136 < 0 && Console.WriteLine("\nLoi: n >= 0 !") != null);
 
-            if (n_136 < 2)
-            {
-                Console.WriteLine("\nn phai >= 2. Xin nhap lai!");
-            }
-        } while (n_136 < 2);
+        P_136 = 1;
+        themang_136 = n_136;
 
-        i_136 = 3;
-        S_136 = Math.Pow(2.0, 1.0 / 2);
-
-        while (i_136 <= n_136)
+        while (themang_136 != 0)
         {
-            S_136 = Math.Pow((i_136 + S_136) * 1.0, 1.0 / i_136);
-            i_136++;
+            P_136 *= themang_136 % 10;
+            themang_136 /= 10;
         }
 
-        Console.WriteLine("\nTong la " + S_136);
+        Console.WriteLine("\nTich la " + P_136);
     }
 }
